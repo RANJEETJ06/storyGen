1cce0c89ce05d0f7621590e36a858dbbf1464817
Ranjeet Kumar Jena <121615244+RANJEETJ06@users.noreply.github.com>
2025-07-27 01:57:50 +0530

    Remove unused Spring Boot test context import from StoryGenApplicationTests

--- src/test/java/com/generator/storygen/StoryGenApplicationTests.java
+++ src/test/java/com/generator/storygen/StoryGenApplicationTests.java
@@ -2,7 +2,6 @@ package com.generator.storygen;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @Disabled
 class StoryGenApplicationTests {
